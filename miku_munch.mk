1c4039e192dd9d0c482d2c0204ab9ff22eaccc5c
imzrme <i@mzrme.com>
2023-09-28 21:20:40 +0000

    Add accidentally deleted .mk file name suffix

--- miku_munch.mk
+++ miku_munch.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common miku stuff.
-$(call inherit-product, vendor/miku/build/product/miku_product)
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/xiaomi/munch/device.mk)
